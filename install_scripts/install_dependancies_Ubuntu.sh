392814004ff034e86090b613fa2e79fbb9d8a46f
M-J-V <malcolmvas@gmail.com>
2022-10-05 01:52:05 +0200

    Installation Shell Script for Ubuntu

---
+++ install_scripts/install_dependancies_Ubuntu.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Make sure that make is installed
+sudo apt-get update -y
+sudo apt-get install bubblewrap build-essential -y
+sudo apt install curl unzip -y
+
+# Set switch name in package
+git clone https://github.com/coq/platform.git
+cd platform/package_picks
+echo COQ_PLATFORM_SWITCH_NAME=coq_for_waterproof >> coq_platform_switch_name.sh
+cd ..
+
+# Make coq_platform
+chmod +x coq_platform_make.sh
+./coq_platform_make.sh -extent=b -parallel=p -jobs=2 -vst=n -compcert=n -pick="8.14~2022.01" -set-switch=y
+
+# Clean up coq/platfrom git
+cd ..
+rm -rf platform
+
+#Install Serapi
+#git clone https://github.com/ejgallego/coq-serapi.git
+#cd coq-serapi
+opam install coq-serapi
+
+
+
