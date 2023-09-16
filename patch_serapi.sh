41a1d605b5c01698c11d17a039b65166558e080b
Jim Portegies <j.w.portegies@tue.nl>
2023-09-16 20:24:02 +0200

    Change base directory name and switch name

--- patch_serapi.sh
+++ patch_serapi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 # Set default install dir
-sed -i 's/  InstallDir "C:\\.*/  InstallDir "C:\\cygwin_coq_platform\\home\\runneradmin\\.opam\\coq_for_waterproof"/g' platform/windows/Coq.nsi
+sed -i 's/  InstallDir "C:\\.*/  InstallDir "C:\\cygwin_wp\\home\\runneradmin\\.opam\\wp"/g' platform/windows/Coq.nsi
 
 # Comment out whitelist to include
 sed -i '/^OPAM_FILE_WHITELIST\[base\]/ c#OPAM_FILE_WHITELIST\[base\]' platform/windows/create_installer_windows.sh
@@ -15,8 +15,8 @@ sed -i 's$cp source/coq-compcert/LICENSE coq-compcert-license.txt$#cp source/coq
 sed -i "s#'\\\n'\"sexplib0\"##g" platform/shell_scripts/installer_create_tree.sh
 
 # Change META files to not uses threading which is not supported on Windows
-sed -i '/^requires(mt,mt_posix)/ c#requires(mt,mt_posix)' .opam/coq_for_waterproof/lib/threads/META
-sed -i '/^type_of_threads = "posix"/ c#type_of_threads = "none"' .opam/coq_for_waterproof/lib/threads/META
+sed -i '/^requires(mt,mt_posix)/ c#requires(mt,mt_posix)' .opam/wp/lib/threads/META
+sed -i '/^type_of_threads = "posix"/ c#type_of_threads = "none"' .opam/wp/lib/threads/META
 
 # Comment out line to ignore modified warnings
 sed -i '/      echo "In package '\''$1'\'' the file '\''$file'\'' does not exist"/!b;n;c#      exit 1' platform/windows/create_installer_windows.sh
